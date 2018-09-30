5720a51d1e0dbf67cbad510055ecefff5fa55275
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-09-29 20:58:47 -0400

    check url

--- promenade/urls.py
+++ promenade/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
 
 urlpatterns = [
+    path('paths/', include('paths.urls')),
     path('admin/', admin.site.urls),
+    
 ]
+
+
