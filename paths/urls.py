5783348a2644840d20e66365570a7b25696f0881
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-09-30 22:03:29 -0400

    test view

--- paths/urls.py
+++ paths/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from paths import views
 
 urlpatterns = [
-    path('', views.index, name='index'), # find out ''
-    
+    path('', views.index, name='index'),
+    path('time/', views.curr_time, name='current_time'),
 ]
--- paths/views.py
+++ paths/views.py
@@ -1,6 +1,17 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django import template
+from django.template.loader import render_to_string
+from datetime import datetime
 
 def index(request):
     return HttpResponse('Hi everybody!!')
 
+def curr_time(request):
+    current_time = datetime.now()
+    
+    # html_time = template.Template('<html><body><div>It is {{current_time}}</div></body></html>')
+    context = template.Context({'current_time': current_time})
+    html_time = render_to_string('paths/test_template.html', context)
+    html = html_time.render(context)
+    return HttpResponse(html)
