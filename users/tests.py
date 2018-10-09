c912c39ee0c62ddbee10b92ae65e0e4f5f86d4b0
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-10-08 20:52:07 -0400

    add auth, tests

--- users/tests.py
+++ users/tests.py
@@ -1,14 +1,75 @@
+from django.contrib.auth.models import User
+# from django.contrib.auth.forms import UserCreationForm
 from django.test import TestCase
 from django.urls import resolve, reverse
+
+from users.forms import SignUpForm
 from users.views import signup
 # Create your tests here.
 
 class SignUpTests(TestCase):
-    def test_signup_status_code(self):
+
+    def setUp(self):
         url = reverse('signup')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.response = self.client.get(url)
+
+    def test_signup_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
 
     def test_signup_url_resolves_signup_view(self):
         view = resolve('/account/signup/')
         self.assertEquals(view.func, signup)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, SignUpForm)
+
+    def test_form_inputs(self):
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
+
+class SuccessfulSignUpTests(TestCase):
+
+    def setUp(self):
+        url = reverse('signup')
+        data = {
+            'username': 'eddie',
+            'email': 'email@somemail.com',
+            'password1': '321qwe321',
+            'password2': '321qwe321'
+        }
+        self.response = self.client.post(url, data)
+        self.distr_url = reverse('districts')
+
+    def test_redirection(self):
+        self.assertRedirects(self.response, self.distr_url)
+
+    def test_user_created(self):
+        self.assertTrue(User.objects.exists())
+
+    def test_user_authenticated(self):
+        response = self.client.get(self.distr_url)
+        user = response.context.get('user')
+        self.assertTrue(user.is_authenticated)
+
+class InvalidSignUpTest(TestCase):
+    
+    def setUp(self):
+        url = reverse('signup')
+        data = {}
+        self.response = self.client.post(url, data)
+
+    def test_signup_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_form_errors(self):
+        form = self.response.context.get('form')
+        self.assertTrue(form.errors)
+
+    def test_dont_create_user(self):
+        self.assertFalse(User.objects.exists())
--- users/views.py
+++ users/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
+
+from users.forms import SignUpForm
 
 # Create your views here.
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
             return redirect('districts')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'users/signup.html', {'form': form})
