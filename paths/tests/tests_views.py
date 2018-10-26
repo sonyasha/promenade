e7977048f9fc4579f0febb794580e597cd90c902
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-10-25 21:12:14 -0400

    test comment

--- paths/tests/tests_views.py
+++ paths/tests/tests_views.py
@@ -3,7 +3,7 @@ from django.urls import resolve, reverse
 from django.test import TestCase
 
 from paths.forms import NewWalkForm
-from paths.views import districts, district_walks, new_walk
+from paths.views import districts, district_walks, new_walk, single_walk
 from paths.models import Neighborhood, GeoWalk
 
 class DistrictTests(TestCase):
@@ -149,3 +149,19 @@ class GeoWalkSlugTest(TestCase):
 
         self.assertEqual(walk_1.slug, 'new-walk')
         self.assertEqual(walk_2.slug, 'new-walk-2')
+
+class SingleWalkTest(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username='john', email='john@doe.com', password='123')
+        # self.client.login(username='john', password='123')
+        district = Neighborhood.objects.create(name='New District', geom={"type":"Polygon","coordinates":[[[-77.01756772064958,38.91401270588173],[-77.01644470054677,38.91424038031397],[-76.99067731309495,38.922984764482244],[-76.98540374355869,38.924580016568626],[-76.97739424316825,38.92868609733962],[-76.96349549748187,38.93524604773688],[-76.94444549114694,38.92048790701407],[-76.92194803559413,38.90284934024322],[-76.90933249357992,38.89286541446984],[-76.93364414255991,38.8738697573288],[-76.9397293611171,38.86900263036611],[-76.94679115896449,38.86372632259586],[-76.97374316347495,38.87502874727069],[-76.97930398849596,38.87760402017108],[-76.98457323322508,38.87668958008691],[-76.99115938635487,38.877262298349606],[-77.00057888424202,38.879705979067644],[-77.00065656095119,38.89393184234142],[-77.00072936169909,38.90725964554979],[-77.00446847513915,38.90898444277938],[-77.01186560306972,38.906123769661235],[-77.01201832579397,38.90731548571515],[-77.01392489176823,38.907374860628515],[-77.01618207078499,38.911487978320864],[-77.01756772064958,38.91401270588173]]]}, slug='newdistr')
+        walk = GeoWalk.objects.create(name='New walk', description='2Lorem ipsum dolor sit amet', geom={'type': 'LineString', 'coordinates': [[-77.033013, 38.033586], [-77.093453, 38.073313], [-77.025013, 38.027586]]}, created_by=user)
+        url = reverse('single_walk', kwargs={'slug': district.slug, 'walk_slug': walk.slug})
+        self.response = self.client.get(url)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+        
+    def test_view_function(self):
+        view = resolve('/districts/newdistr/new-walk/')
+        self.assertEquals(view.func, single_walk)
